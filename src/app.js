c5f1d4afdf10090f416b58ce2ec0b0a5200c6d2b
Nicolas <nicolas.venturini@student.vinci.be>
2023-12-06 15:30:39 +0100

    first commit and change readMe

---
+++ src/app.js
@@ -0,0 +1,123 @@
+function greet(name) {
+
+    if (!Array.isArray(name)) {
+        if (checkNameNotDefined(name)) {
+            return 'Hello, my friend.';
+        }
+
+        if (checkUpperCase(name)) {
+            return 'HELLO, ' + name + '!';
+        }
+
+        if (!checkUpperCase(name)) {
+            return 'Hello, ' + name + '.';
+        }
+    } else {
+        return checkAllNames(name);
+    }
+}
+
+function checkAllNames(array) {
+
+    const arrayLower = array.filter((x) => !checkUpperCase(x));
+
+    let language;
+    const languageVerif = arrayLower[arrayLower.length-1];
+    if(languageVerif == 'fr' || languageVerif == 'nl' || languageVerif == 'en'){
+        language = languageVerif;
+        arrayLower.pop();
+    }
+
+    const stringLower = checkAllLowerNames(arrayLower,language);
+
+    const arrayUpper = array.filter((x) => checkUpperCase(x));
+
+    const stringUpper = checkAllUpperNames(arrayUpper,language);
+
+    return stringLower + stringUpper;
+}
+
+function checkNameNotDefined(name) {
+    return name === undefined || name === ' ' || name === null;
+}
+
+function checkUpperCase(name) {
+    return name.toUpperCase() === name;
+}
+
+
+function checkAllUpperNames(arrayUpper,language) {
+    let stringUpper = '';
+
+    if (arrayUpper.length > 0) {
+       stringUpper =  checkUpperLanguage(language);
+        for (let i = 0; i < arrayUpper.length; i++) {
+            if (i == arrayUpper.length - 1) {
+                stringUpper += ' ' + arrayUpper[i] + '!';
+            } else {
+                stringUpper += ' ' + arrayUpper[i] + ',';
+            }
+        }
+    }
+    return stringUpper;
+}
+
+function checkUpperLanguage(language){
+
+    let stringUpper;
+
+    if(language == 'fr'){
+        stringUpper = ' ET BONJOUR';
+    }
+    else if(language == 'nl'){
+        stringUpper = ' EN GOEIEDAG';
+    }else{
+        stringUpper = ' AND HELLO';
+    }
+
+    return stringUpper;
+}
+
+function checkAllLowerNames(arrayLower,language) {
+    let stringLower;
+    let andWord;
+
+    if (arrayLower.length > 0) {
+        let result = checkLowerLanguage(language);
+        stringLower = result.stringLower;
+        andWord = result.andWord;
+        for (let i = 0; i < arrayLower.length; i++) {
+            if (i == arrayLower.length - 1 && arrayLower.length > 1) {
+                stringLower += andWord + arrayLower[i];
+            } else {
+                stringLower += ', ' + arrayLower[i];
+            }
+        }
+    }
+
+    return stringLower + '.';
+}   
+
+function checkLowerLanguage(language){
+    let stringLower;
+    let andWord;
+
+    if(language == 'fr'){
+        andWord = ' et ';
+        stringLower = 'Bonjour';
+    }
+    else if(language == 'nl'){
+        andWord = ' en ';
+        stringLower = 'Goeiedag';
+    }else{
+        andWord = ' and ';
+        stringLower = 'Hello';
+    }
+
+    return {stringLower,andWord};
+}
+
+
+
+
+module.exports = greet;
---
+++ src/app.test.js
@@ -0,0 +1,123 @@
+const greet = require('./app');
+
+test('print name', () => {
+    const result = greet('Bob');
+    expect(result).toBe('Hello, Bob.');
+});
+
+test('check name empty', () => {
+    const result = greet(' ');
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('check name null', () => {
+    const result = greet(null);
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('check name undefined', () => {
+    const result = greet(undefined);
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('check upper name', () => {
+    const result = greet('JERRY');
+    expect(result).toBe('HELLO, JERRY!');
+});
+
+test('check double name', () => {
+    const result = greet(['Jill', 'Jane']);
+    expect(result).toBe('Hello, Jill and Jane.');
+});
+
+test('check triple name', () => {
+    const result = greet(['Amy', 'Brian', 'Charlotte']);
+    expect(result).toBe('Hello, Amy, Brian and Charlotte.');
+});
+
+test('check mutiple names upper and lower case', () => {
+    const result = greet(['Amy', 'BRIAN', 'Charlotte']);
+    expect(result).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN!');
+});
+
+test('check mutiple names upper and lower case', () => {
+    const result = greet(['Amy', 'BRIAN', 'Charlotte', 'ALEXIS']);
+    expect(result).toBe('Hello, Amy and Charlotte. AND HELLO BRIAN, ALEXIS!');
+});
+
+test('check multiple language name fr (Lower)', () => { 
+    const result = greet(['Kratos', 'Thanatos','fr']);
+    expect(result).toBe('Bonjour, Kratos et Thanatos.');
+});
+
+test('check multiple language name nl (Lower)', () => { 
+    const result = greet(['Kratos', 'Thanatos','nl']);
+    expect(result).toBe('Goeiedag, Kratos en Thanatos.');
+});
+
+test('check multiple language name en (lower)', () => { 
+    const result = greet(['Kratos', 'Thanatos','en']);
+    expect(result).toBe('Hello, Kratos and Thanatos.');
+});
+
+test('check multiple language name fr (lower and UpperCase)', () =>{
+    const result = greet (['Kratos', 'THANATOS', 'fr']);
+    expect(result).toBe('Bonjour, Kratos. ET BONJOUR THANATOS!');
+});
+
+test('check multiple language name en (lower and UpperCase)', () =>{
+    const result = greet (['Kratos', 'THANATOS', 'en']);
+    expect(result).toBe('Hello, Kratos. AND HELLO THANATOS!');
+});
+
+test('check multiple language name nl (lower and UpperCase)', () =>{
+    const result = greet (['Kratos', 'THANATOS', 'nl']);
+    expect(result).toBe('Goeiedag, Kratos. EN GOEIEDAG THANATOS!');
+});
+
+// test du prof 
+
+test('test 1 du prof', () => { 
+    const result = greet('Jean-Kevin');
+    expect(result).toBe('Hello, Jean-Kevin.');
+});
+
+test('test 2 a du prof', () => { 
+    const result = greet(null);
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('test 2 b du prof', () => { 
+    const result = greet(undefined);
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('test 2 c du prof', () => { 
+    const result = greet(' ');
+    expect(result).toBe('Hello, my friend.');
+});
+
+test('test 3 du prof', () => { 
+    const result = greet('KENOBI');
+    expect(result).toBe('HELLO, KENOBI!');
+});
+
+test('test 4 du prof', () => { 
+    const result = greet(["Kratos", "Thanatos"]);
+    expect(result).toBe('Hello, Kratos and Thanatos.');
+});
+
+test('test 5 du prof', () => { 
+    const result = greet(["Kratos", "Thanatos", "Hypnos"]);
+    expect(result).toBe('Hello, Kratos, Thanatos and Hypnos.');
+});
+
+test('test 6 du prof', () => { 
+    const result = greet(["KRATOS", "Thanatos", "Hypnos"]);
+    expect(result).toBe('Hello, Thanatos and Hypnos. AND HELLO KRATOS!');
+});
+
+test('test 7 du prof', () => { 
+    const result = greet(["Kratos", "Thanatos", "fr"]);
+    expect(result).toBe('Bonjour, Kratos et Thanatos.');
+});
